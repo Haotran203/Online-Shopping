c4bc84acc76ab2ab9102ca59c17aaf7a34b2d820
Haotran203 <quanghaotran303@gmail.com>
2025-04-11 18:05:23 +0700

    Completed Project Year2Term2

---
+++ app/src/main/java/com/example/onlineshopping/activities/PaymentActivity.java
@@ -0,0 +1,108 @@
+package com.example.onlineshopping.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.onlineshopping.R;
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+public class PaymentActivity extends AppCompatActivity implements PaymentResultListener {
+
+    double amount = 0.0;
+    Toolbar toolbar;
+    TextView subTotal, discount, shipping, total;
+    Button paymentByn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+        //Toolbar
+        toolbar = findViewById(R.id.payment_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        amount = getIntent().getDoubleExtra("amount", 0.0);
+
+        subTotal = findViewById(R.id.sub_total);
+        discount = findViewById(R.id.textView17);
+        shipping = findViewById(R.id.textView18);
+        total = findViewById(R.id.total_amt);
+        paymentByn = findViewById(R.id.pay_btn);
+
+        subTotal.setText(amount + " VND");
+        total.setText(amount + " VND");
+
+        paymentByn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paymentMethod();
+            }
+        });
+
+
+    }
+
+    private void paymentMethod() {
+
+        Checkout checkout = new Checkout();
+
+        final Activity activity = PaymentActivity.this;
+
+        try {
+            JSONObject options = new JSONObject();
+            //Set Company Name
+            options.put("name", "IPHONE STORE");
+            //Ref no
+            options.put("description", "Reference No. #123456");
+            //Image to be display
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            //options.put("order_id", "order_9A33XWu170gUtm");
+            // Currency type
+            options.put("currency", "USD");
+            //double total = Double.parseDouble(mAmountText.getText().toString());
+            //multiply with 100 to get exact amount in rupee
+            amount = amount * 100;
+            //amount
+            options.put("amount", amount);
+            JSONObject preFill = new JSONObject();
+            //email
+            preFill.put("email", "haotq.21it@vku.udn.vn");
+            //contact
+            preFill.put("contact", "0396079303");
+
+            options.put("prefill", preFill);
+
+            checkout.open(activity, options);
+        } catch (Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+    @Override
+    public void onPaymentSuccess(String s) {
+        Toast.makeText(this, "Thanh toán thành công !", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        Toast.makeText(this, "Đã hủy thanh toán !", Toast.LENGTH_SHORT).show();
+    }
+}
